1b6b1d3527d9402b2666e4eed75dcbd026134bae
lex <lex@users.noreply.github.com>
2018-02-16 20:20:04 +0200

    tweak coordinates to include more decimals

--- observations/admin.py
+++ observations/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ObservationPoint, Observation
+
+admin.site.register(ObservationPoint)
+admin.site.register(Observation)
--- observations/models.py
+++ observations/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class ObservationPoint(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=10, decimal_places=7)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
 
 
 class Observation(models.Model):
