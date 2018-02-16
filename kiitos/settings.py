468233b1d50a0e8b41aa7903d12b627245033ca8
lex <lex@users.noreply.github.com>
2018-02-16 19:31:54 +0200

    set DEBUG from envvar

--- kiitos/settings.py
+++ kiitos/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yes keep secret please'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1)) == 1
 
 ALLOWED_HOSTS = []
 
